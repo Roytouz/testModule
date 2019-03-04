ed8e2b02bac7b3ca9211955e394f2ac71c6fc354
Roy <roy.dourng@pathmazing.com>
2019-03-04 14:16:55 +0700

    Initial Commit

---
+++ testModule/testModule.h
@@ -0,0 +1,19 @@
+//
+//  testModule.h
+//  testModule
+//
+//  Created by roy on 3/4/19.
+//  Copyright © 2019 Pathmazing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testModule.
+FOUNDATION_EXPORT double testModuleVersionNumber;
+
+//! Project version string for testModule.
+FOUNDATION_EXPORT const unsigned char testModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testModule/PublicHeader.h>
+
+
